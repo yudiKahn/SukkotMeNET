16d5f2cfcd9d35cf600acd93663ae3bf3f3a06d8
Yehuda Kahn <yehuda@nogacs.com>
2022-08-05 00:40:41 +0300

    small fixes

---
+++ SukkotMeNET/Models/PageLayoutBase.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Components;
+using SukkotMeNET.Services;
+
+namespace SukkotMeNET.Models
+{
+    public class PageLayoutBase : LayoutComponentBase
+    {
+        bool _UsedOnAfterRender;
+
+        [Inject]
+        public MainStateService MainService { get; set; }
+
+        public void Dispose()
+        {
+            MainService.StateHasChanged -= InvokeStateHasChanged;
+        }
+
+        public bool SetProperty<T>(ref T prop, T val)
+        {
+            if (EqualityComparer<T>.Default.Equals(prop, val))
+                return false;
+
+            prop = val;
+            StateHasChanged();
+            return true;
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+
+            if (_UsedOnAfterRender) return;
+
+            _UsedOnAfterRender = true;
+            if (firstRender)
+                MainService.StateHasChanged += InvokeStateHasChanged;
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (_UsedOnAfterRender) return;
+
+            _UsedOnAfterRender = true;
+            if (firstRender)
+            {
+                MainService.StateHasChanged += InvokeStateHasChanged;
+            }
+        }
+
+        void InvokeStateHasChanged(object? sender, EventArgs args) => InvokeAsync(StateHasChanged);
+    }
+}
