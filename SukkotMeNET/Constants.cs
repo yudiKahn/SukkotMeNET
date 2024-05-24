81bc91b8704dbf89be89ae404fd0cfe8dc817059
YehudaKahn <yudikahn8@gmail.com>
2024-05-24 10:04:20 +0300

    basic stoch chart. admin create user

--- SukkotMeNET/Constants.cs
+++ SukkotMeNET/Constants.cs
@@ -18,10 +18,12 @@ namespace SukkotMeNET
             public const string Confirm = "confirm";
             public const string Prompt = "prompt";
             public const string GetImageFromHTML = "GetImageFromHTML";
+            public const string RenderStockChart = "RenderStockChart";
         }
 
         public class General
         {
+            //todo:
             public const string IsraeliSetItemId = "6176e416dae028e3337ce552";
             public const string YaneverSetItemId = "6176e51e654d28089f656e19";
         }
